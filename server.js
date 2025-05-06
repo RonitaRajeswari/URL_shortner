ed806ead27ca4d286a3ba13184b7b8ed1cba03ca
RonitaRajeswari <162408056+RonitaRajeswari@users.noreply.github.com>
2025-05-06 14:52:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import  {shortUrl, getOriginalUrl} from './controllers/url.js'
 
 const app = express();
 
+
+app.use(express.urlencoded({extended:true}))
 mongoose.connect("mongodb+srv://rajeswarimahapatra40:onXaow2NFUJ125M6@cluster0.oliutah.mongodb.net/",{
-    dbName:"nodeJS Learn"
+    dbName:"nodeJSLearn"
 }).then(() => console.log("MongoDb Connected ..!")).catch((err) => console.log(err))
 
 
 app.get('/',(req,res) => {
     res.render("index.ejs", {shortUrl : null})
 })
+
+
+//shorting url logic
+app.post('/short', shortUrl)
+
+//redirect to original url using shor code :- dynamic routing
+app.get("/:shortCode", getOriginalUrl)
+
 
 const port = 3000;
 app.listen(port,()=>console.log(`server is running on port ${port}`))
